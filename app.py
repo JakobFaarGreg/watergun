1f2e5d0cc93b04e583300ca3473c761a39e74338
Jakob Faarbæk Gregersen <lilleper.jfg@gmail.com>
2026-03-05 21:06:46 +0000

    chore: add todo and types

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # This scripts calculates the Volumetric Flow Rate of a watergun
 import pandas as pd # type: ignore
 import print_utils as pu
+import math
 
 df = pd.read_csv("raw_water_time.csv")
 
@@ -9,11 +10,18 @@ VOLUMETRIC_FLOW_RATE: str = "volumetric_flow_rate"
 CENTILITER: str = "centiliter"
 SECONDS: str = "seconds"
 
+diameter_in_millimeter: float = 1
+diameter_in_meter: float = diameter_in_millimeter / 1000
+cross_sectional_area_in_meter: float = math.pi * math.pow(diameter_in_meter / 2, 2)
+# TODO: follow the thread, i figure out how to get to velocity, and from there to force.
+
+
 # Calculate VFR per row
 df[CUBIC_METER] = df[CENTILITER] * 0.000001
 df[VOLUMETRIC_FLOW_RATE] = df[CUBIC_METER] / df[SECONDS]
 
 # Remove outliers and calculate aritmetic mean
+# TODO: ensure this is proper outlier detection. If not, improve or remove this outlier handling.
 df = df.drop(df[VOLUMETRIC_FLOW_RATE].idxmax())
 df = df.drop(df[VOLUMETRIC_FLOW_RATE].idxmin())
 
