4d567bbaeeb66e3db15b672abb92bd376b8a8411
Jakob Faarbæk Gregersen <lilleper.jfg@gmail.com>
2026-03-05 17:18:04 +0000

    feat: calculate vfr

---
+++ app.py
@@ -0,0 +1,17 @@
+# This scripts calculates the Volumetric Flow Rate of a watergun
+import pandas as pd
+import print_utils as pu
+
+df = pd.read_csv("raw_water_time.csv")
+
+# Calculate VFR per row
+df["cubicmeter"] = df["cl"] * 0.000001
+df["volumetric_flow_rate"] = df["cubicmeter"] / df["time"]
+
+# Remove outliers and calculate aritmetic mean
+df = df.drop(df["volumetric_flow_rate"].idxmax())
+df = df.drop(df["volumetric_flow_rate"].idxmin())
+
+# Print mean and persist table
+pu.pretty_print(str.format("Volumetric Flow Rate: {}", df["volumetric_flow_rate"].mean()))
+df.to_csv("volumetric_flow_rate.csv", index=False)
---
+++ print_utils.py
@@ -0,0 +1,8 @@
+def pretty_print(print_string: str):
+    line = "=" * len(print_string)
+    print(line)
+    print(print_string)
+    print(line)
+
+if __name__ == "__main__":
+    exit(0)
