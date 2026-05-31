8c81ce6b6c035bcf42c9c8da1cdfff6bc0c14c30
Jakob Faarbæk Gregersen <lilleper.jfg@gmail.com>
2026-05-31 12:02:03 +0000

    feat: update classigier to show top 5 predictions, names and ids

--- classifier/app.py
+++ classifier/app.py
@@ -1,14 +1,37 @@
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 from PIL import Image
 import torch
+from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "index",
+    type=int,
+    help="Index of the image to classify"
+)
+args = parser.parse_args()
+
+# Get all images from the directory
+image_dir = Path("/workspace/png/doves")
+images = sorted(image_dir.glob("*.jpg"))
+
+if args.index < 0 or args.index >= len(images):
+    raise IndexError(
+        f"Image index {args.index} is out of range. "
+        f"There are {len(images)} images."
+    )
+
+image_path = images[args.index]
+
+print(f"Using image #{args.index}: {image_path}")
 
 model_id = "chriamue/bird-species-classifier"
 
 processor = AutoImageProcessor.from_pretrained(model_id)
 model = AutoModelForImageClassification.from_pretrained(model_id)
 
-#image = Image.open("/workspaces/watergun/png/doves/20260319_173350.jpg")
-image = Image.open("/workspaces/watergun/png/doves/20260325_063841.jpg")
+image = Image.open(image_path)
 
 inputs = processor(images=image, return_tensors="pt")
 
@@ -16,6 +39,11 @@ with torch.no_grad():
     outputs = model(**inputs)
 
 logits = outputs.logits
-predicted_class_id = logits.argmax(dim=-1).item()
+probs = torch.nn.functional.softmax(logits, dim=-1)[0]
+top5_probs, top5_ids = torch.topk(probs, 5)
 
-print("Class ID:", predicted_class_id)
+print("Top 5 predictions:")
+for score, class_id in zip(top5_probs, top5_ids):
+    class_id = class_id.item()
+    label = model.config.id2label[class_id]
+    print(f"{class_id:3d}: {label:30s} {score.item():.2%}")
