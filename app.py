a0126ebaee17e11694aeb41bd76631cd92a1ee42
Jakob Faarbæk Gregersen <lilleper.jfg@gmail.com>
2026-07-16 13:18:58 +0000

    feat: classifies on latest.jpg when docker compose runs

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ def job():
     print(f"{time.localtime()} Running classifier on latest image")
     classifyImage(Path("/workspace/img/snapshots/latest.jpg"))
 
-schedule.every.minute.do(job)
+schedule.every().minute.do(job)
 
 if __name__ == "__main__":
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
